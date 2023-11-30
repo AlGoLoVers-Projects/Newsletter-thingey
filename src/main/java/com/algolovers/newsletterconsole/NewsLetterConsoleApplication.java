a505b033a2d13a1638a810b6ef2ef85bae7dcd08
sooryasrajan <soorya.s27@gmail.com>
2023-12-01 00:03:42 +0530

    First commit

---
+++ src/main/java/com/algolovers/newsletterconsole/NewsLetterConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.algolovers.newsletterconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsLetterConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsLetterConsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algolovers/newsletterconsole/NewsLetterConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algolovers.newsletterconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsLetterConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
