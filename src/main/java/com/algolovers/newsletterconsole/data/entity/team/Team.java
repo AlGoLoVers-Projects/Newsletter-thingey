42897787131ee8754d651013ed7fff9859591112
sooryasrajan <soorya.s27@gmail.com>
2023-12-05 01:49:34 +0530

    Added team entities

---
+++ src/main/java/com/algolovers/newsletterconsole/data/entity/team/Team.java
@@ -0,0 +1,37 @@
+package com.algolovers.newsletterconsole.data.entity.team;
+
+import com.algolovers.newsletterconsole.data.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Table(name = "team")
+@Entity
+@Data
+public class Team {
+
+    @Id
+    @JsonIgnore
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    @Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "Username can only contain alphanumeric characters and underscores")
+    @Size(min = 3, max = 30, message = "Username must have a minimum of three characters and maximum of 30 characters")
+    String teamName;
+
+    @ManyToOne
+    @JoinColumn(name = "team_owner_id", nullable = false)
+    private User teamOwner;
+
+    @OneToMany(mappedBy = "team")
+    private List<TeamMember> teamMembers;
+
+    //TODO: Add questions
+    //private List<Questions> questions;
+
+}
---
+++ src/main/java/com/algolovers/newsletterconsole/data/entity/team/TeamMember.java
@@ -0,0 +1,28 @@
+package com.algolovers.newsletterconsole.data.entity.team;
+
+import com.algolovers.newsletterconsole.data.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Table(name = "team_member")
+@Entity
+@Data
+public class TeamMember {
+
+    @Id
+    @JsonIgnore
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", nullable = false)
+    private Team team;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private boolean hasEditAccess;
+}
+
