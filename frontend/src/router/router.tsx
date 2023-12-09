fd4f4fec049abd50655db127e124c7d73b7687fc
sooryasrajan <soorya.s27@gmail.com>
2023-12-10 01:39:48 +0530

    Added new react frontend

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,18 @@
+import {createBrowserRouter} from "react-router-dom";
+import {paths} from "./paths";
+import App from "../App";
+
+export const router = createBrowserRouter([
+    {
+        path: paths.home,
+        element: <App/>,
+    },
+    {
+        path: paths.login,
+        element: <div>Login</div>,
+    },
+    {
+        path: paths.signup,
+        element: <div>Singup</div>,
+    },
+]);
