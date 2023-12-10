1030737d04720d22c76a3b2aa73e85e8813f90ef
sooryasrajan <soorya.s27@gmail.com>
2023-12-11 00:10:10 +0530

    Fixed title

--- frontend/src/pages/authentication/SignUp.tsx
+++ frontend/src/pages/authentication/SignUp.tsx
@@ -50,8 +50,11 @@ export default function SignUp() {
                     <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
                         <LockOutlinedIcon/>
                     </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
+                    <Typography component="h1" variant="h5" sx={{
+                        fontWeight: 'bold',
+                        textAlign: 'center'
+                    }}>
+                        Sign up to Newsletter
                     </Typography>
                     <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
                         <Grid container spacing={2}>
