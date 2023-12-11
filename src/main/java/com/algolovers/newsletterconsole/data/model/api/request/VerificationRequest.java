45faa1c7dcea74bd0b7a9b35f02a25039cdd58de
sooryasrajan <soorya.s27@gmail.com>
2023-12-12 00:50:37 +0530

    Added validation to verification request

--- src/main/java/com/algolovers/newsletterconsole/data/model/api/request/VerificationRequest.java
+++ src/main/java/com/algolovers/newsletterconsole/data/model/api/request/VerificationRequest.java
@@ -1,13 +1,17 @@
 package com.algolovers.newsletterconsole.data.model.api.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class VerificationRequest {
-    @NotEmpty
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
     private String email;
-    @NotNull
+
+    @NotNull(message = "Verification code cannot be null")
     private Long verificationCode;
 }
