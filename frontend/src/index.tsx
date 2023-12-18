1149fbf52e109bbfd880739773077e30b32a698d
sooryasrajan <soorya.s27@gmail.com>
2023-12-19 00:40:24 +0530

    Added persist gate

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,11 @@ import './index.css';
 import {BrowserRouter} from 'react-router-dom'
 import App from "./App";
 import {Provider} from "react-redux";
-import store from "./redux/store";
+import store, {persist} from "./redux/store";
 import {ThemeProvider} from "@mui/material";
 import {buildTheme} from "./theme/theme";
 import {ToastContainer} from "react-toastify";
+import {PersistGate} from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -15,12 +16,14 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <ThemeProvider theme={buildTheme()}>
-                    <ToastContainer/>
-                    <App/>
-                </ThemeProvider>
-            </BrowserRouter>
+            <PersistGate persistor={persist}>
+                <BrowserRouter>
+                    <ThemeProvider theme={buildTheme()}>
+                        <ToastContainer/>
+                        <App/>
+                    </ThemeProvider>
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
