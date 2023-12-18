22f335070e6e1a23a18aa81a3cb4802cedcbb566
sooryasrajan <soorya.s27@gmail.com>
2023-12-19 00:43:39 +0530

    Added redux serializable checks

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,8 +1,16 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {persistReducer, persistStore} from "redux-persist";
-import {rootReducer} from "./reducer";
-import {PersistConfig} from "redux-persist/es/types";
-import storage from "redux-persist/lib/storage";
+import {configureStore,} from '@reduxjs/toolkit';
+import {persistReducer, persistStore} from 'redux-persist';
+import {rootReducer} from './reducer';
+import {PersistConfig} from 'redux-persist/es/types';
+import storage from 'redux-persist/lib/storage';
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+} from 'redux-persist/es/constants';
 
 const persistConfig: PersistConfig<any> = {
     key: 'root',
@@ -16,7 +24,9 @@ const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(
         {
-            serializableCheck: false,
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            }
         }
     ).concat(),
 });
