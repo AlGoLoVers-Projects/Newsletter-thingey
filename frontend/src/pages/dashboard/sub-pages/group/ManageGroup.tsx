db1e1ba9ac467f17a7390d6ac6b5d198eb9c5362
sooryasrajan <soorya.s27@gmail.com>
2023-12-24 23:04:14 +0530

    Built separate view for owner/group member

--- frontend/src/pages/dashboard/sub-pages/group/ManageGroup.tsx
+++ frontend/src/pages/dashboard/sub-pages/group/ManageGroup.tsx
@@ -19,16 +19,63 @@ import {
     updateGroupName
 } from "../../../../redux/rootslices/data/groups.slice";
 import {useDispatch, useSelector} from "react-redux";
+import {selectUserData} from "../../../../redux/rootslices/data/auth-data.slice";
 
 export default function ManageGroup(): React.ReactElement {
     const {state} = useLocation();
-    const navigate = useNavigate()
-    const dispatch = useDispatch();
     const groupId = state as string;
 
     const groupData: GroupData = useSelector(
         (state) => selectGroupByIdMemoized(state, groupId)
     ) ?? {} as GroupData;
+
+    const userEmailAddress = useSelector(selectUserData).emailAddress
+    const isGroupOwner = groupData.groupOwner.emailAddress === userEmailAddress
+
+    return (
+        <Container
+            component="main"
+            disableGutters
+            sx={{
+                padding: 2,
+                margin: 0,
+                display: "flex",
+                flexDirection: "column",
+                position: 'relative',
+                minWidth: "100%",
+                flex: 1
+            }}
+        >
+            <Typography
+                component="h1"
+                variant="h2"
+                sx={{
+                    fontWeight: 'bold',
+                    alignSelf: "flex-start",
+                    fontSize: {
+                        xs: '1rem',
+                        sm: '2rem',
+                        xl: '3rem',
+                    },
+                }}
+            >
+                {isGroupOwner ? `Manage ${groupData.groupName}` : `View ${groupData.groupName}`}
+            </Typography>
+            {isGroupOwner ? <RenderOwnerGroup groupData={groupData} /> : <RenderMemberGroup groupData={groupData}/>}
+        </Container>
+    )
+}
+
+function RenderMemberGroup(props: {groupData: GroupData}): React.ReactElement {
+    const groupData = props.groupData;
+
+    return (<div></div>)
+}
+
+function RenderOwnerGroup(props: {groupData: GroupData}): React.ReactElement {
+    const groupData = props.groupData;
+    const navigate = useNavigate()
+    const dispatch = useDispatch();
 
     const [editGroup, {isLoading}] = useEditGroupMutation()
     const [deleteGroup, {isLoading: isDeleting}] = useDeleteGroupMutation()
@@ -113,149 +160,74 @@ export default function ManageGroup(): React.ReactElement {
     }
 
     return (
-        <Container
-            component="main"
-            disableGutters
-            sx={{
-                padding: 2,
-                margin: 0,
-                display: "flex",
-                flexDirection: "column",
-                position: 'relative',
-                minWidth: "100%",
-                flex: 1
-            }}
+        <Box
+            maxWidth="xl"
         >
-            <Typography
-                component="h1"
-                variant="h2"
+            <Card
                 sx={{
-                    fontWeight: 'bold',
-                    alignSelf: "flex-start",
-                    fontSize: {
-                        xs: '1rem',
-                        sm: '2rem',
-                        xl: '3rem',
-                    },
+                    mt: 3,
+                    p: 3,
+                    maxWidth: "100%",
+                    borderRadius: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
                 }}
             >
-                Manage {groupData.groupName}
-            </Typography>
-            <Box
-                maxWidth="xl"
-            >
-                <Card
-                    sx={{
-                        mt: 3,
-                        p: 3,
-                        maxWidth: "100%",
-                        borderRadius: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <Typography component="h1" variant="h6" sx={{
-                        fontWeight: 'bold',
-                    }}>
-                        Update Group Information
-                    </Typography>
+                <Typography component="h1" variant="h6" sx={{
+                    fontWeight: 'bold',
+                }}>
+                    Update Group Information
+                </Typography>
 
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            mt: 3,
-                        }}
-                        component="form"
-                        onSubmit={handleSubmit}
-                        noValidate
-                    >
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            value={groupName}
-                            id="group-name"
-                            label="Group Name"
-                            name="group-name"
-                            type="text"
-                            autoComplete="organization"
-                            autoFocus
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                setGroupName(e.target.value)
-                            }}
-                            error={!isEmpty(groupNameError)}
-                            helperText={groupNameError}
-                        />
-                        <TextField
-                            rows={4}
-                            multiline
-                            margin="normal"
-                            required
-                            fullWidth
-                            value={groupDesc}
-                            name="description"
-                            label="Description"
-                            type="text"
-                            id="description"
-                            autoComplete="text"
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                                setGroupDesc(e.target.value)
-                            }}
-                            error={!isEmpty(groupDescError)}
-                            helperText={groupDescError}
-                        />
-                        <Box sx={{
-                            flex: 1
-                        }}>
-                        </Box>
-                        <Box sx={{
-                            display: "flex",
-                            alignSelf: "end",
-                            flexDirection: "row",
-                            gap: 2,
-                        }}>
-                            <Button
-                                type="button"
-                                variant="outlined"
-                                disabled={isDeleting || isLoading}
-                                onClick={() => {
-                                    setGroupName(groupData.groupName)
-                                    setGroupDesc(groupData.groupDescription)
-                                }}
-                                sx={{mt: 3, mb: 1}}
-                            >
-                                Reset
-                            </Button>
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                disabled={isDeleting || isLoading}
-                                sx={{mt: 3, mb: 1}}
-                            >
-                                Update Group Information
-                            </Button>
-                        </Box>
-                    </Box>
-                </Card>
-                <Card
+                <Box
                     sx={{
+                        display: "flex",
+                        flexDirection: "column",
                         mt: 3,
-                        p: 3,
-                        maxWidth: "100%",
-                        borderRadius: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
                     }}
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
                 >
-                    <Typography component="h1" variant="h6" sx={{
-                        fontWeight: 'bold',
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        value={groupName}
+                        id="group-name"
+                        label="Group Name"
+                        name="group-name"
+                        type="text"
+                        autoComplete="organization"
+                        autoFocus
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setGroupName(e.target.value)
+                        }}
+                        error={!isEmpty(groupNameError)}
+                        helperText={groupNameError}
+                    />
+                    <TextField
+                        rows={4}
+                        multiline
+                        margin="normal"
+                        required
+                        fullWidth
+                        value={groupDesc}
+                        name="description"
+                        label="Description"
+                        type="text"
+                        id="description"
+                        autoComplete="text"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setGroupDesc(e.target.value)
+                        }}
+                        error={!isEmpty(groupDescError)}
+                        helperText={groupDescError}
+                    />
+                    <Box sx={{
+                        flex: 1
                     }}>
-                        Manage Users
-                    </Typography>
-                    <Typography variant="body2">
-                        Add or remove existing users, revoke or grant editing permission and manage your group invitations.
-                    </Typography>
+                    </Box>
                     <Box sx={{
                         display: "flex",
                         alignSelf: "end",
@@ -263,58 +235,104 @@ export default function ManageGroup(): React.ReactElement {
                         gap: 2,
                     }}>
                         <Button
-                            type="submit"
-                            variant="contained"
-                            disabled={isLoading}
-                            sx={{mt: 3, mb: 1}}
+                            type="button"
+                            variant="outlined"
+                            disabled={isDeleting || isLoading}
                             onClick={() => {
-                                navigate(authorizedPaths.manageGroupUsers, {state: groupData.id})
+                                setGroupName(groupData.groupName)
+                                setGroupDesc(groupData.groupDescription)
                             }}
-                            endIcon={<NavigateNext />}
+                            sx={{mt: 3, mb: 1}}
                         >
-                            Manage Users
+                            Reset
                         </Button>
-                    </Box>
-                </Card>
-                <Card
-                    sx={{
-                        mt: 3,
-                        p: 3,
-                        maxWidth: "100%",
-                        borderRadius: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <Typography component="h1" variant="h6" sx={{
-                        fontWeight: 'bold',
-                    }}>
-                        Delete Group
-                    </Typography>
-                    <Typography variant="body2">
-                        Deleting the group will remove all information including group specific questions, users
-                        associated with the groups and all responses. Proceed with caution.
-                    </Typography>
-                    <Box sx={{
-                        display: "flex",
-                        alignSelf: "end",
-                        flexDirection: "row",
-                        gap: 2,
-                    }}>
                         <Button
                             type="submit"
                             variant="contained"
                             disabled={isDeleting || isLoading}
                             sx={{mt: 3, mb: 1}}
-                            onClick={() => {
-                                handleDeletion()
-                            }}
                         >
-                            Delete Group
+                            Update Group Information
                         </Button>
                     </Box>
-                </Card>
-            </Box>
-        </Container>
+                </Box>
+            </Card>
+            <Card
+                sx={{
+                    mt: 3,
+                    p: 3,
+                    maxWidth: "100%",
+                    borderRadius: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                <Typography component="h1" variant="h6" sx={{
+                    fontWeight: 'bold',
+                }}>
+                    Manage Users
+                </Typography>
+                <Typography variant="body2">
+                    Add or remove existing users, revoke or grant editing permission and manage your group invitations.
+                </Typography>
+                <Box sx={{
+                    display: "flex",
+                    alignSelf: "end",
+                    flexDirection: "row",
+                    gap: 2,
+                }}>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        disabled={isLoading}
+                        sx={{mt: 3, mb: 1}}
+                        onClick={() => {
+                            navigate(authorizedPaths.manageGroupUsers, {state: groupData.id})
+                        }}
+                        endIcon={<NavigateNext />}
+                    >
+                        Manage Users
+                    </Button>
+                </Box>
+            </Card>
+            <Card
+                sx={{
+                    mt: 3,
+                    p: 3,
+                    maxWidth: "100%",
+                    borderRadius: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                <Typography component="h1" variant="h6" sx={{
+                    fontWeight: 'bold',
+                }}>
+                    Delete Group
+                </Typography>
+                <Typography variant="body2">
+                    Deleting the group will remove all information including group specific questions, users
+                    associated with the groups and all responses. Proceed with caution.
+                </Typography>
+                <Box sx={{
+                    display: "flex",
+                    alignSelf: "end",
+                    flexDirection: "row",
+                    gap: 2,
+                }}>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        disabled={isDeleting || isLoading}
+                        sx={{mt: 3, mb: 1}}
+                        onClick={() => {
+                            handleDeletion()
+                        }}
+                    >
+                        Delete Group
+                    </Button>
+                </Box>
+            </Card>
+        </Box>
     )
 }
