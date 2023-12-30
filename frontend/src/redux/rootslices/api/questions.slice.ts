98bba1e9e86fabccc0e46e7316c4a60204cb5e62
sooryasrajan <soorya.s27@gmail.com>
2023-12-30 15:34:43 +0530

    Added questions slice

---
+++ frontend/src/redux/rootslices/api/questions.slice.ts
@@ -0,0 +1,11 @@
+import {baseApiSlice} from "./base.slice";
+
+export const invitationsSlice = baseApiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+
+    }),
+});
+
+export const {
+
+} = invitationsSlice;
