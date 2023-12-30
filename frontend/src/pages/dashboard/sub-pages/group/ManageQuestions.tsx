c6bf3318ed05d65ce8dbd567a8734c13fa0ad533
sooryasrajan <soorya.s27@gmail.com>
2023-12-30 15:59:10 +0530

    Added Questions Provider to ManageQuestions

--- frontend/src/pages/dashboard/sub-pages/group/ManageQuestions.tsx
+++ frontend/src/pages/dashboard/sub-pages/group/ManageQuestions.tsx
@@ -8,6 +8,7 @@ import {useSelector} from "react-redux";
 import {selectGroupByIdMemoized} from "../../../../redux/rootslices/data/groups.slice";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
+import {QuestionsProvider, useQuestions} from "../../../../components/elements/QuestionsProvider";
 
 export default function ManageQuestions(): React.ReactElement {
     const {state} = useLocation();
@@ -17,103 +18,108 @@ export default function ManageQuestions(): React.ReactElement {
         (state) => selectGroupByIdMemoized(state, groupId)
     ) ?? {} as GroupData;
 
+    const {state: questionsState, dispatch: questionsDispatch} = useQuestions();
+
     return (
-        <Container
-            component="main"
-            disableGutters
-            sx={{
-                padding: 2,
-                margin: 0,
-                display: "flex",
-                flexDirection: "column",
-                position: 'relative',
-                minWidth: "100%",
-                flex: 1
-            }}
-        >
-            <Typography
-                component="h1"
-                variant="h2"
+        <QuestionsProvider>
+            <Container
+                component="main"
+                disableGutters
                 sx={{
-                    fontWeight: 'bold',
-                    alignSelf: "flex-start",
-                    fontSize: {
-                        xs: '1rem',
-                        sm: '2rem',
-                        xl: '3rem',
-                    },
+                    padding: 2,
+                    margin: 0,
+                    display: "flex",
+                    flexDirection: "column",
+                    position: 'relative',
+                    minWidth: "100%",
+                    flex: 1
                 }}
             >
-                {groupData.groupName}
-            </Typography>
-            <Box
-                maxWidth="xl"
-            >
-                <Card
+                <Typography
+                    component="h1"
+                    variant="h2"
                     sx={{
-                        mt: 3,
-                        p: 3,
-                        maxWidth: "100%",
-                        borderRadius: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
+                        fontWeight: 'bold',
+                        alignSelf: "flex-start",
+                        fontSize: {
+                            xs: '1rem',
+                            sm: '2rem',
+                            xl: '3rem',
+                        },
                     }}
                 >
-                    <Box sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        flexDirection: "row",
-                    }}>
-                        <Typography component="h1" variant="h6" sx={{
-                            fontWeight: 'bold',
+                    {groupData.groupName}
+                </Typography>
+                <Box
+                    maxWidth="xl"
+                >
+                    <Card
+                        sx={{
+                            mt: 3,
+                            p: 3,
+                            maxWidth: "100%",
+                            borderRadius: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <Box sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                            flexDirection: "row",
                         }}>
-                            Manage Questions
+                            <Typography component="h1" variant="h6" sx={{
+                                fontWeight: 'bold',
+                            }}>
+                                Manage Questions
+                            </Typography>
+                            <Button
+                                variant="contained"
+                                color="success"
+                                endIcon={<AddIcon/>}
+                            >
+                                New Question
+                            </Button>
+                        </Box>
+                        <Typography variant="body2">
+                            Curate, add or modify questions tailored for your group so you can stay in touch with
+                            eachother.
                         </Typography>
-                        <Button
-                            variant="contained"
-                            color="success"
-                            endIcon={<AddIcon/>}
-                        >
-                            New Question
-                        </Button>
-                    </Box>
-                    <Typography variant="body2">
-                        Curate, add or modify questions tailored for your group so you can stay in touch with eachother.
-                    </Typography>
-                    <Box sx={{
-                        display: "flex",
-                        mt: 4
-                    }}>
+                        <Box sx={{
+                            display: "flex",
+                            mt: 4
+                        }}>
 
-                        {/*TODO: Render existing questions here*/}
-                        {/*TODO: Create slice to maintain questions*/}
-                        {/*TODO: Create provider for questions. ADD, UPDATE, REORDER INDEX, DELETE*/}
+                            {/*TODO: Render existing questions here*/}
+                            {/*TODO: Create slice to maintain questions*/}
+                            {/*TODO: Create provider for questions. ADD, UPDATE, REORDER INDEX, DELETE*/}
 
-                    </Box>
-                    <Box sx={{
-                        display: "flex",
-                        alignSelf: "end",
-                        flexDirection: "row",
-                        gap: 2,
-                    }}>
-                        <Button
-                            type="button"
-                            variant="outlined"
-                            sx={{mt: 3, mb: 1}}
-                        >
-                            Reset
-                        </Button>
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            sx={{mt: 3, mb: 1}}
-                        >
-                            Update Questions
-                        </Button>
-                    </Box>
-                </Card>
-            </Box>
-        </Container>
+                        </Box>
+                        <Box sx={{
+                            display: "flex",
+                            alignSelf: "end",
+                            flexDirection: "row",
+                            gap: 2,
+                        }}>
+                            <Button
+                                type="button"
+                                variant="outlined"
+                                sx={{mt: 3, mb: 1}}
+                            >
+                                Reset
+                            </Button>
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                sx={{mt: 3, mb: 1}}
+                            >
+                                Update Questions
+                            </Button>
+                        </Box>
+                    </Card>
+                </Box>
+            </Container>
+        </QuestionsProvider>
     )
 }
