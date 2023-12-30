82086c0384395e120933d07450a3fba2665497ba
sooryasrajan <soorya.s27@gmail.com>
2023-12-30 13:33:44 +0530

    Added sort for questions by index

--- src/main/java/com/algolovers/newsletterconsole/service/QuestionsService.java
+++ src/main/java/com/algolovers/newsletterconsole/service/QuestionsService.java
@@ -16,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -116,7 +117,11 @@ public class QuestionsService {
                 return new Result<>(true, null, "You are not part of this group");
             }
 
-            List<Question> questions = group.getQuestions();
+            List<Question> questions =
+                    group.getQuestions()
+                            .stream()
+                            .sorted(Comparator.comparingInt(Question::getQuestionIndex))
+                            .toList();
 
             return new Result<>(true, questions, "Questions fetched successfully");
         } catch (Exception e) {
