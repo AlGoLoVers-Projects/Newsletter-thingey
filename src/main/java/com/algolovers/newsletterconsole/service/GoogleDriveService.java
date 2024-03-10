d3ac75ab94abc4831dab7b92bf71478548650cf2
sooryasrajan <soorya.s27@gmail.com>
2024-03-11 00:08:35 +0530

    Moved google drive config Removed imageIds cloudinaryService -> replaced with Google Drive service
    Added GoogleDriveService

--- src/main/java/com/algolovers/newsletterconsole/service/GoogleDriveService.java
+++ src/main/java/com/algolovers/newsletterconsole/service/GoogleDriveService.java
@@ -1,35 +1,122 @@
 package com.algolovers.newsletterconsole.service;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.drive.DriveScopes;
-import com.google.auth.http.HttpCredentialsAdapter;
-import com.google.auth.oauth2.GoogleCredentials;
-import org.springframework.context.annotation.Bean;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
+import com.google.api.client.http.ByteArrayContent;
 import com.google.api.services.drive.Drive;
-import org.springframework.context.annotation.Configuration;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import com.google.api.services.drive.model.Permission;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
-@Configuration
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class GoogleDriveService {
 
-    private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String SERVICE_ACCOUNT_FILE_PATH = "src/main/resources/newsletter-407514-eaafe38e3dcd.json";
+    private final Drive driveService;
 
-    @Bean
-    public Drive driveService() throws IOException, GeneralSecurityException {
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(SERVICE_ACCOUNT_FILE_PATH))
-                .createScoped(Collections.singleton(DriveScopes.DRIVE));
-
-        return new Drive.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JSON_FACTORY,
-                new HttpCredentialsAdapter(credentials))
-                .setApplicationName("Newsletter console")
-                .build();
+    public String getPublicUrl(File file) {
+        return "https://drive.google.com/thumbnail?id=" + file.getId();
     }
+
+    public File uploadFile(String folderName, String fileName, String mimeType, byte[] fileBytes) {
+        try {
+            String folderId = getOrCreateFolderByName(folderName);
+            return uploadFileToGoogleDrive(fileBytes, fileName, mimeType, folderId);
+        } catch (IOException e) {
+            log.error("Failed to upload file: {} for group: {}. Mime-type: {}", fileName, folderName, mimeType, e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void replaceImageInFolder(String folderName, String existingImageName, byte[] newImageFileContent) throws IOException {
+        // Find the existing image file in the folder
+        String folderId = getOrCreateFolderByName(folderName);
+        String query = "mimeType='image/jpeg' and '" + folderId + "' in parents and name='" + existingImageName + "'";
+        FileList result = driveService.files().list().setQ(query).execute();
+        List<File> files = result.getFiles();
+        if (files != null && !files.isEmpty()) {
+            // Delete the existing image file
+            driveService.files().delete(files.get(0).getId()).execute();
+        }
+        // Upload the new image file to the same folder
+        uploadFileToGoogleDrive(newImageFileContent, existingImageName, "image/jpeg", folderId);
+    }
+
+    public String deleteFolderByName(String folderName) {
+        try {
+            // Find the folder by name
+            String query = "mimeType='application/vnd.google-apps.folder' and name='" + folderName + "'";
+            FileList result = driveService.files().list().setQ(query).execute();
+            for (File file : result.getFiles()) {
+                // Delete the folder
+                driveService.files().delete(file.getId()).execute();
+                return folderName;
+            }
+            return null;
+        } catch (IOException e) {
+            log.error("Failed to delete folder: {}", folderName, e);
+            return null;
+        }
+    }
+
+    private String getFolderIdByName(String folderName) throws IOException {
+        String query = "mimeType='application/vnd.google-apps.folder' and name='" + folderName + "'";
+        List<File> files = driveService.files().list().setQ(query).execute().getFiles();
+        if (files != null && !files.isEmpty()) {
+            return files.get(0).getId();
+        }
+        return null;
+    }
+
+
+    private String getOrCreateFolderByName(String folderName) throws IOException {
+        String folderId = getFolderIdByName(folderName);
+        if (folderId == null) {
+            folderId = createFolder(folderName);
+        }
+        return folderId;
+    }
+
+    private String createFolder(String folderName) throws IOException {
+        File folderMetadata = new File();
+        folderMetadata.setName(folderName);
+        folderMetadata.setMimeType("application/vnd.google-apps.folder");
+
+        File folder = driveService.files().create(folderMetadata)
+                .setFields("id")
+                .execute();
+        return folder.getId();
+    }
+
+    private File uploadFileToGoogleDrive(byte[] fileContent, String fileName, String mimeType, String folderId)
+            throws IOException {
+        File fileMetadata = new File();
+        fileMetadata.setName(fileName);
+        fileMetadata.setMimeType(mimeType);
+        if (folderId != null) {
+            fileMetadata.setParents(Collections.singletonList(folderId));
+        }
+
+        ByteArrayContent mediaContent = new ByteArrayContent(mimeType, fileContent);
+
+        // Upload file
+        File uploadedFile = driveService.files().create(fileMetadata, mediaContent)
+                .setFields("id, name, webViewLink") // Request fields including webViewLink (public URL)
+                .execute();
+
+        // Set file visibility to public
+        Permission permission = new Permission();
+        permission.setType("anyone");
+        permission.setRole("reader");
+        driveService.permissions().create(uploadedFile.getId(), permission).execute();
+
+        return uploadedFile;
+    }
+
 }
