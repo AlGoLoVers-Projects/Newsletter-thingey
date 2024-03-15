7ea1ad9aaab9b628f0ab3ae365dbaab26f5839b1
sooryasrajan <soorya.s27@gmail.com>
2024-03-16 02:44:37 +0530

    Added ehcache config Added manual queries

--- src/main/java/com/algolovers/newsletterconsole/repository/GroupRepository.java
+++ src/main/java/com/algolovers/newsletterconsole/repository/GroupRepository.java
@@ -3,6 +3,7 @@ package com.algolovers.newsletterconsole.repository;
 import com.algolovers.newsletterconsole.data.entity.groups.Group;
 import com.algolovers.newsletterconsole.data.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +11,10 @@ import java.util.List;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, String> {
 
+    @Query("SELECT g FROM Group g JOIN FETCH g.groupMembers gm WHERE gm.user = :user")
     List<Group> findByGroupMembersUser(User user);
 
+    @Query("SELECT g FROM Group g JOIN g.groupMembers gm WHERE gm.user = :user AND g.acceptQuestionResponse = :acceptQuestionResponse")
     List<Group> findByGroupMembersUserAndAcceptQuestionResponse(User groupMembers_user, boolean acceptQuestionResponse);
 
 }
