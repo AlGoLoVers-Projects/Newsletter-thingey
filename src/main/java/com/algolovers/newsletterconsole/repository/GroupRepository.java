0be7679ce428cfc9b5fc9328a1af9a93859077f0
sooryasrajan <soorya.s27@gmail.com>
2024-03-17 00:37:51 +0530

    Removed queries

--- src/main/java/com/algolovers/newsletterconsole/repository/GroupRepository.java
+++ src/main/java/com/algolovers/newsletterconsole/repository/GroupRepository.java
@@ -3,7 +3,6 @@ package com.algolovers.newsletterconsole.repository;
 import com.algolovers.newsletterconsole.data.entity.groups.Group;
 import com.algolovers.newsletterconsole.data.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,10 +10,8 @@ import java.util.List;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, String> {
 
-    @Query("SELECT g FROM Group g JOIN FETCH g.groupMembers gm WHERE gm.user = :user")
     List<Group> findByGroupMembersUser(User user);
 
-    @Query("SELECT g FROM Group g JOIN g.groupMembers gm WHERE gm.user = :user AND g.acceptQuestionResponse = :acceptQuestionResponse")
     List<Group> findByGroupMembersUserAndAcceptQuestionResponse(User user, boolean acceptQuestionResponse);
 
 }
--- src/main/java/com/algolovers/newsletterconsole/repository/UserRepository.java
+++ src/main/java/com/algolovers/newsletterconsole/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.algolovers.newsletterconsole.repository;
 
 import com.algolovers.newsletterconsole.data.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-    @Query(value = "SELECT u FROM User u WHERE u.emailAddress = :username")
     Optional<User> findByEmailAddress(String username);
 
     Boolean existsByEmailAddress(String email);
